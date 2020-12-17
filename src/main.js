9293f70abd424c6ec3260d904eb21a37584ff409
ayayaElf <1550252538@qq.com>
2020-12-17 08:45:05 +0800

    增加测试页面

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
-import { createApp } from 'vue'
+import {
+    createApp
+} from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// import ElementUI from 'element-ui'
+// import 'element-ui/lib/theme-chalk/index.css'
+// import CollapseTransition from 'element-ui/lib/transitions/collapse-transition'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
+createApp(App).use(VueAxios, axios)
 createApp(App).use(store).use(router).mount('#app')
